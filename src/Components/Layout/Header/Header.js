9ada72bfeef041e991854787ab0a00fdf8b3cebd
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-07-26 10:37:01 +0530

    Day-2: Adding MealsSummay is finished

--- src/Components/Layout/Header/Header.js
+++ src/Components/Layout/Header/Header.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react'
 import styles from "./Header.module.css"
 import mealImage from '../../../Assets/meals.jpg';
 import HeaderCartButton from '../HeaderCartButton/HeaderCartButton';
+import Meals from '../../Meals/Meals';
 
 const Header = (props) => {
     return (
@@ -13,6 +14,7 @@ const Header = (props) => {
             <div className={styles.mainImage}>
                 <img src={mealImage} alt="A table full of delicious food!" />
             </div>
+            <Meals />
         </Fragment>
     )
 }
---
+++ src/Components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+import MealsSummary from "./MealsSummary/MealsSummary";
+
+const Meals = () => {
+    return (
+        <Fragment>
+            <MealsSummary />
+        </Fragment>
+    )
+}
+export default Meals;
